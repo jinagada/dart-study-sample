fd69a7e845238d8564426e1282401782f5205350
jinagada <kimyg3174@gmail.com>
2020-12-28 17:03:34 +0900

    Dart 예제 테스트

--- bin/sample_test.dart
+++ bin/sample_test.dart
@@ -1,5 +1,27 @@
 import 'package:sample_test/sample_test.dart' as sample_test;
+import 'package:sample_test/sample_test.dart' show TestA;
+
+int set(int a, int b) {
+  return a + b;
+}
+
+int yield(int a, int b) {
+  return a + b;
+}
+
+void test() async {
+  // var await = 10; // var error
+}
 
 void main(List<String> arguments) {
   print('Hello world: ${sample_test.calculate()}!');
+  dynamic name = 'Kim';
+  print('String : ${name}');
+  name = 100;
+  print('int : ${name}');
+  print('set(10, 5) : ${set(10, 5)}');
+  var test = TestA();
+  test.show();
+  var await = 11;
+  print('Function yield(11, 12) : ${yield(await, 12)}');
 }
--- lib/sample_test.dart
+++ lib/sample_test.dart
@@ -1,3 +1,15 @@
 int calculate() {
   return 6 * 7;
 }
+
+class TestA {
+  void show() {
+    print('TestA');
+  }
+}
+
+class TestB {
+  void show() {
+    print('TestB');
+  }
+}
